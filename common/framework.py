5a654f795f9e367f432a5e757784c3b52e09dcf9
asmcos <asmcos@gmail.com>
2022-05-23 09:27:03 +0800

    股票分组已经完成

--- common/framework.py
+++ common/framework.py
@@ -46,6 +46,47 @@ class API():
         url = self.host + "/getfactors"
         return requests.get(url,params={'factorname':factorname,'date':date})
 
+    def create_group(self,name,description):
+        url = self.host + "/groupmanagers"
+        return requests.post(url,params={"name":name,"description":description})
+
+    #为了解决参数顺序问题，设置了默认参数None，但是设置None是不允许的。
+    def add_togroup(self,name=None,code=None,date=None,vol=None):
+        if name is None or code is None or date is None or vol is None:
+            return "need more params"
+
+        url = self.host + "/grouplists"
+        return requests.post(url,params={"name":name,
+            "code":code,
+            "vol":vol,
+            "status":1,
+            "date":date})
+
+    
+    def del_fromgroup(self,name=None,code=None,date=None,vol=None):
+        if name is None or code is None or date is None or vol is None:
+            return "need more params"
+
+        url = self.host + "/grouplists"
+        return requests.post(url,params={"name":name,
+            "code":code,
+            "vol":vol,
+            "status":-1,
+            "date":date})
+    def codes_fromgroup(self,name,start,end):
+        url = self.host + "/grouplists"
+        resp = requests.get(url,params={"name":name,"start":start,"end":end})
+        if resp.status_code != 200:
+            return resp
+
+        # 去掉重复语句
+        codelist = []
+        result = []
+        for i in resp.json():
+            if i['code'] not in codelist:
+                result.append(i)
+            codelist.append(i['code'])
+        return result
 
 
 
--- group.py
+++ group.py
@@ -1,18 +1,19 @@
 #
 # Kdata 组接口
 #
+# 注意：⚠️ 组结算时按照提交的先后顺序即时结算的。如果提交时间错乱会导致无法计算。
+#
 from common.framework import API
 
 kapi = API()
 
 kapi.create_group("建筑","目前建筑板块正在启动")
 
-#code = "sh.601012" #测试数据
 code = "sz.003214" #测试数据
-kapi.add_togroup("建筑",code,date="2022-05-03",vol=1000)
+kapi.add_togroup("建筑",code,date="2022-05-04",vol=1000)
 
 code = "sh.601012" #测试数据
-kapi.add_togroup("建筑",code,date="2022-05-03",vol=1000)
+kapi.add_togroup("建筑",code,date="2022-05-04",vol=1000)
 
 code = "sh.601012" #测试数据
 
@@ -21,4 +22,7 @@ kapi.del_fromgroup("建筑",code,date="2022-05-04",vol=200)
 code = "sz.003214" #测试数据
 kapi.del_fromgroup("建筑",code,date="2022-05-04",vol=200)
 
-#resp = kapi.get_group("高领持仓",start="2003-02-01",end="2021-10-15")
+resp = kapi.codes_fromgroup("建筑",start="2010-02-01",end="2022-10-15")
+if isinstance(resp,list):
+    for i in resp:
+        print(i)
