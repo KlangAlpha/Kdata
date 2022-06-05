2703bd4f90607bd3ed051058f3eb43e449536ce7
asmcos <asmcos@gmail.com>
2022-06-05 19:03:29 +0800

    增加setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+ Kdata setup file
+"""
+
+from setuptools import setup
+
+setup(
+    name="Kdata",
+    version="0.2.0",
+    url="https://github.com/KlangAlpha/Kdata",
+    author="asmcos",
+    author_email="asmcos@jeapedu.com",
+    packages=['Kdata'],
+    package_dir={"Kdata":"common"},
+)
