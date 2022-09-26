7e181fdbbbeb1203d3d67496ffb5a3659b77c3e7
asmcos <asmcos@gmail.com>
2022-09-26 09:18:05 +0800

    增加前复权数据 baostock

---
+++ bs_day.py
@@ -0,0 +1,46 @@
+#pytdx
+
+import pandas as pd
+from common.framework import API 
+from common.common import * 
+import json,os
+import requests
+import baostock as bs
+
+bs.login()
+
+start_d = get_date(15)
+
+session = ""
+
+
+def get_bar(name,code):
+    bdata = bs.query_history_k_data_plus(code,'date,open,high,low,close,volume,amount,turn,code', start_date=start_d,frequency='d',adjustflag="2")
+    datas = bdata.get_data()
+    print(name,code,end=" ")
+    print(len(datas),datas.iloc[-1].date)
+    #datas = datas [datas['volume'] > 0]
+    df = datas.to_json(orient='table')
+    jsondatas = json.loads(df)['data']
+    for d in jsondatas:
+        d['name'] = name
+        del d['index']
+    #print(jsondatas)
+    #print(datas.iloc[-1],liutonggu,d)
+
+    try:
+        resp = session.post(hostname+"/dayks/updates",json=jsondatas,timeout=2000)
+    except:
+        time.sleep(2)
+        session.post(hostname+"/dayks/updates",json=jsondatas,timeout=2000)
+
+
+if True:
+
+    kapi = API()
+
+    stocklist = kapi.get_stocklist().json()
+    session = requests.Session()
+    for stock in stocklist[:]:
+        code ,name = stock['code'],stock['name']    
+        get_bar(name,code)
