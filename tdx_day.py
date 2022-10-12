1b9242f91f1de3d0d22327a645386463ed4842f6
asmcos <asmcos@gmail.com>
2022-10-12 17:24:28 +0800

    修改tdx仅仅为日更

--- tdx_day.py
+++ tdx_day.py
@@ -22,11 +22,13 @@ def get_bar(name,code):
         zone = 1
     
     print(name,code1)
-    datas = api.get_security_bars(9,zone,code1, 0, 400)
+    datas = api.get_security_bars(9,zone,code1, 0, 1)
     info = api.get_finance_info(zone, code1)  
     datas = api.to_df(datas)
-    if len(datas) < 2:
+    """
+    if len(datas) < 1:
         return
+    """
 
     liutonggu = float(info['liutongguben'])
     datas = datas.assign(date=datas['datetime'].apply(lambda x: str(x)[0:10])).drop(['year', 'month', 'day', 'hour', 'minute', 'datetime'], axis=1)
@@ -44,13 +46,15 @@ def get_bar(name,code):
         del d['index']
     #print(jsondatas)
     #print(datas.iloc[-1],liutonggu,d)
+    
     try:
         resp = session.post(hostname+"/dayks/updates",json=jsondatas,timeout=2000)
     except:
         time.sleep(2)
         session.post(hostname+"/dayks/updates",json=jsondatas,timeout=2000)
 
-if api.connect('119.147.212.81', 7709):
+#if api.connect('119.147.212.81', 7709):
+if api.connect('8.142.137.74', 7709):
 
     kapi = API()
 
